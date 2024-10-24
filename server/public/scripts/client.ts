7114148bb7ae78c75bc2f6350a16bacf8eaee2d0
Jacob Larson <hockeybobber@comcast.net>
2024-10-24 16:54:21 -0500

    added get route and imported axios

--- server/public/scripts/client.ts
+++ server/public/scripts/client.ts
@@ -1,1 +1,18 @@
+import axios from "./axios";
+
 console.log('client.ts is sourced!');
+
+
+
+//the GET route for the math
+function getCalc() {
+    axios({
+        url: '/calculations',
+        method: 'GET'
+      }).then((response) => {
+        let calculations = response.data 
+        renderingMath(calculations)
+        console.log('this data is being rendered', calculations);
+      })
+  
+  }
