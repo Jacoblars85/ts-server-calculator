e22c990111704d90a4dd1f41ea7dc891aa541435
Jacob Larson <hockeybobber@comcast.net>
2024-10-25 22:39:46 -0500

    made the new post and fixed the render

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -18,19 +18,42 @@ function renderingMath(calculations) {
     var recentResult = document.getElementById('recentResult');
     var resultHistory = document.getElementById('resultHistory');
     //clearing previous spot for this area
-    // resultHistory.innerHTML = ''
-    // recentResult.innerHTML = '';
+    resultHistory.innerHTML = '';
+    recentResult.innerHTML = '';
     //   adds to the newest math that was just done
-    //     recentResult.innerHTML += `
-    //     <b>${calculations[calculations.length-1].result}</b>
-    //   `
+    recentResult.innerHTML += "\n        <b>".concat(calculations[calculations.length - 1].result, "</b>\n      ");
     for (var _i = 0, calculations_1 = calculations; _i < calculations_1.length; _i++) {
         var calc = calculations_1[_i];
         //should add all of the recent resaults into this part
-        //   resultHistory.innerHTML +=`
-        //   <ol>${calc.numOne} ${calc.operator} ${calc.numTwo} = ${calc.result}</ol>
-        //   `
+        resultHistory.innerHTML += "\n          <ol>".concat(calc.numOne, " ").concat(calc.operator, " ").concat(calc.numTwo, " = ").concat(calc.result, "</ol>\n          ");
     }
+    ;
+}
+function postMath(event) {
+    event.preventDefault();
+    //grabs the input and makes it a variable
+    var numOne = document.getElementById('numOne');
+    var numTwo = document.getElementById('numTwo');
+    operatorButton(event);
+    //puts the input into an object 
+    var newMath = {
+        numOne: numOne.valueAsNumber,
+        operator: operator,
+        numTwo: numTwo.valueAsNumber,
+        result: result
+    };
+    console.log(newMath);
+    axios({
+        method: 'POST',
+        url: '/calculations',
+        data: newMath
+    }).then(function (response) {
+        console.log('did this work?');
+        getCalc();
+    });
+    //clears the inputs
+    numOne.value = '';
+    numTwo.value = '';
 }
 //figures out which operator got clicked
 function operatorButton(event, op) {
