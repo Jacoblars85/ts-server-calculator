37370a0b4b17ed94a31db5a2484635a43d63d7c9
Jacob Larson <hockeybobber@comcast.net>
2024-10-28 21:11:55 -0500

    new post and calc function

--- server/server.js
+++ server/server.js
@@ -11,13 +11,41 @@ var calculations = [{
         numTwo: 2,
         result: 4,
     }];
-// Here's a wonderful place to make some routes:
+//does all the math
+function doingCalc(numOne, numTwo, operator) {
+    if (operator === '+') {
+        return Number(numOne) + Number(numTwo);
+    }
+    else if (operator === '-') {
+        return Number(numOne) - Number(numTwo);
+    }
+    else if (operator === '*') {
+        return Number(numOne) * Number(numTwo);
+    }
+    else if (operator === '/') {
+        return Number(numOne) / Number(numTwo);
+    }
+}
 // GET /calculations
 app.get('/calculations', function (req, res) {
     console.log('we got a get req', calculations);
     res.send(calculations);
 });
 // POST /calculations
+app.post('/calculations', function (req, res) {
+    console.log('we got a post req');
+    var newMath = req.body;
+    console.log(newMath);
+    //pushes the data into the array
+    calculations.push(newMath);
+    //gets the last object in that array
+    var newestResult = calculations[calculations.length - 1];
+    //does the math
+    newestResult.result = doingCalc(newestResult.numOne, newestResult.numTwo, newestResult.operator);
+    console.log('this should be the result', newestResult.result);
+    console.log('this should be the calc array', calculations);
+    res.sendStatus(201);
+});
 // PLEASE DO NOT MODIFY ANY CODE BELOW THESE BEARS:
 // 🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸
 // This starts the server...but also stores it in a variable.
